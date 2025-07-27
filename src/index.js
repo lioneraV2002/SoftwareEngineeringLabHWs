df6100a04458a63cdb8f3f2b2fdc9c129636bbb7
lioneraV2002 <kamyar_maham1381@yahoo.com>
2025-07-27 23:45:54 +0330

    Modify index.js basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { ThemeProvider } from './context/ThemeContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/SoftwareEngineeringLabHWs'>
       <ThemeProvider>
         <App />
       </ThemeProvider>
